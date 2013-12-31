6f2064a345eb9e6d38e7e94e15001dcbe3d208c5
Nextcoin <nb@nextcoinbox.org>
2013-12-31 17:42:20 +0100

    updated script downloading blocks and transactions file

--- scripts/nextcoinbox-live.sh
+++ scripts/nextcoinbox-live.sh
@@ -2,7 +2,7 @@
 #set -x
 
 file_url='http://info.nxtcrypto.org/nxt-client-0.4.8.zip'
-expected_sha='ec7c30a100717e60d8abe50eedb23641952847d91ff90b9b05a74ff98d8a4cf2  nxt-client-0.4.8.zip'
+expected_sha='ec7c30a100717e60d8abe50eedb23641952847d91ff90b9b05a74ff98d8a4cf2	nxt-client-0.4.8.zip'
 welcome_url='http://nextcoinbox.github.io/nextcoinbox/live_homepage.html'
 
 if ! BROWSER=$(which firefox >/dev/null 2>/dev/null) ; then
@@ -20,11 +20,20 @@ downloaded_file=${file_url##*/}
 echo "Downloading file"
 wget $file_url
 
-if  echo $expected_sha | sha1sum -c ; then
+if  echo $expected_sha | sha256sum -c ; then
   # check is fine, we can continue
   echo "file validity checked"
   echo "Decompressing archive"
   unzip -qq $downloaded_file
+  cd nxt
+  wget https://github.com/nextcoinbox/nextcoinbox/raw/staging/files/blocks.nxt.gz
+  gunzip blocks.nxt.gz
+  wget https://github.com/nextcoinbox/nextcoinbox/raw/staging/files/transactions.nxt.gz
+  gunzip transactions.nxt.gz
+  wget https://github.com/nextcoinbox/nextcoinbox/raw/staging/files/web.xml.gz
+  gunzip web.xml.gz
+  mv web.xml webapps/root/WEB-INF/
+  cd ..
   echo "Starting nxt server"
   (cd nxt && java -jar start.jar >server.log 2>&1 &)&
   while ! netstat -nlp | grep 7875 ; do
